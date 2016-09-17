039689547985109ac59a95341ac1028e762f2f2e
Michael <michael@example.com>
2016-09-17 16:56:19 +0800

    Try to merge

--- MyLife/app/src/main/java/ntu/com/mylife/view/MessageView.java
+++ MyLife/app/src/main/java/ntu/com/mylife/view/MessageView.java
@@ -5,42 +5,26 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import ntu.com.mylife.R;
-import ntu.com.mylife.common.entity.databaseentity.Message;
 
 public class MessageView extends AppCompatActivity {
 
-    public static class MessageViewHolder extends RecyclerView.ViewHolder {
-        public TextView mMessageTextView;
-        public TextView mRespondentTextView;
-        public TextView mTimeTextView;
-
-        public MessageViewHolder(View v) {
-            super(v);
-            mMessageTextView = (TextView)v.findViewById(R.id.messageRespondentTextView);
-            mRespondentTextView = (TextView) v.findViewById(R.id.respondentTextView);
-            mTimeTextView = (TextView) v.findViewById(R.id.timeTextView);
-        }
-    }
+    private Button mSendButton;
+    private RecyclerView mMessageRecyclerView;
+    private LinearLayoutManager mLinearLayoutManager;
+    //private FirebaseRecyclerAdapter<Message, MessageRecyclerViewAdapter.ViewHolder> mFirebaseAdapter;
+    private ProgressBar mProgressBar;
 
     private EditText mMessageEditText;
 
-    private static String MESSAGE_CHILD = "Message";
-
     private DatabaseReference mDatabaseReference;
-    private FirebaseRecyclerAdapter<Message, MessageViewHolder> firebaseRecyclerAdapter;
-    private RecyclerView mMessageRecyclerView;
-    private LinearLayoutManager mLinearLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,48 +33,18 @@ public class MessageView extends AppCompatActivity {
 
         mDatabaseReference = FirebaseDatabase.getInstance().getReference();
 
-        if (mDatabaseReference == null) {
+        if(mDatabaseReference == null) {
             Log.e("MessageView", "Error Leh");
-        } else {
+        }
+        else {
             Log.d("Inside Reference", mDatabaseReference.toString());
         }
 
-        mMessageRecyclerView = (RecyclerView) findViewById(R.id.messageRecyclerView);
-        mLinearLayoutManager = new LinearLayoutManager(this);
-        mLinearLayoutManager.setStackFromEnd(true);
 
-        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Message, MessageViewHolder>(
-                Message.class,
-                R.layout.message,
-                MessageViewHolder.class,
-                mDatabaseReference.child(MESSAGE_CHILD)) {
-            @Override
-            protected void populateViewHolder(MessageViewHolder viewHolder, Message model, int position) {
-                viewHolder.mMessageTextView.setText(model.getMessage());
-                viewHolder.mRespondentTextView.setText(model.getSenderUsername());
-                viewHolder.mTimeTextView.setText(model.getDate());
-            }
-        };
 
-        firebaseRecyclerAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
-            @Override
-            public void onItemRangeInserted(int positionStart, int itemCount) {
-                super.onItemRangeInserted(positionStart, itemCount);
-                int messageCount = firebaseRecyclerAdapter.getItemCount();
-                int lastVisiblePosition = mLinearLayoutManager.findLastCompletelyVisibleItemPosition();
 
-                if (lastVisiblePosition == -1 ||
-                        (positionStart >= (messageCount - 1) && lastVisiblePosition == (positionStart - 1))) {
-                    mMessageRecyclerView.scrollToPosition(positionStart);
-                }
-            }
-        });
-
-        mMessageRecyclerView.setLayoutManager(mLinearLayoutManager);
-        mMessageRecyclerView.setAdapter(firebaseRecyclerAdapter);
-
-        mMessageEditText = (EditText) findViewById(R.id.inputEditText);
 
     }
+
 
 }
