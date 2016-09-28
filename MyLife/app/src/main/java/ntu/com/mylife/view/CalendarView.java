067fc097a54d4dce38e71e48aff7fdfd7ab4023c
Michael <michael@example.com>
2016-09-29 01:01:54 +0800

    try to merge

--- MyLife/app/src/main/java/ntu/com/mylife/view/CalendarView.java
+++ MyLife/app/src/main/java/ntu/com/mylife/view/CalendarView.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
